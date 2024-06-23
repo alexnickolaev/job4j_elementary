fe21ba988550369ab4490065f672f5ea3b774bfc
alexnickolaev <alexnik22@gmail.com>
2024-06-23 19:15:35 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexander Nikolaev");
+        System.out.println("18.12.1987");
+    }
+}
